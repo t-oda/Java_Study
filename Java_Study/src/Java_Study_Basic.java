39cb5a11c87b39d237ae9752e88d40ddddff6147
t-oda <t-oda@NPC741.seraku.jp>
2020-05-14 13:58:38 +0900

    first commit

---
+++ Java_Study/src/Java_Study_Basic.java
@@ -0,0 +1,18 @@
+
+public class Java_Study_Basic {
+
+/**
+ * Java基本学習のためのテストクラス
+ * @author odatoru
+ */
+
+    /**
+     * Hello Worldを出力するメソッド
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Hello Worldを出力
+        System.out.println("Hello World!");
+    }
+}
+
